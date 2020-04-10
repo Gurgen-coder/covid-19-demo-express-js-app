ad6838d2a8220bbcd001ed90ce1dfe3ea27d575f
Harvey Ramer <hramer@ducks.org>
2020-04-09 21:47:21 -0500

    A working Web app.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const app = express();
 const port = 3000;
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.locals.basedir = __dirname;
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use('/', routes);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
 // Home page route.
-router.get('/', function (req, res) {
-  res.send('Hello World. Learn <a href="/about">about this app</a>');
-})
+router.get('/', (req, res) => {
+  const apiUrl = "https://api.covid19api.com/summary";
+  const countries = axios.get(apiUrl).then((response) => {
+      res.render("home", {
+        appName: "My COVID-19 Tracker",
+        pageName: "COVID-19 Cases",
+        data: response.data.Countries,
+      });
+    })
+    .catch(function (err) {
+      return console.error(err);
+    });
+});
 
 // About page route.
 router.get('/about', function (req, res) {
-  res.send('About this COVID-19 Tracker. Go <a href="/">home</a>');
-})
+  res.render("about", {
+    appName: "My COVID-19 Tracker",
+    pageName: "About",
+  });
+});
 
 module.exports = router;
