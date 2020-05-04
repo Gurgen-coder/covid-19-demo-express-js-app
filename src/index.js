56c10d154766ca1d0653cd7d57c74fb1c65dd962
Harvey A. Ramer <harvey@harveyramer.com>
2020-05-03 21:54:08 -0500

    Tutorial 2 (#2)
    * Config changes for app deployment.
    
    Co-authored-by: Harvey Ramer <hramer@ducks.org>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
